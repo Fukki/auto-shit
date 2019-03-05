ec5119be39e764cafae31f8237696517c8029422
Fukki <takungwong@gmail.com>
2019-03-05 17:50:26 +0700

    :x

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ module.exports = function autoShit(mod) {
 		if(arg1 && arg1.length > 0) arg1 = arg1.toLowerCase();
 		if(arg2 && arg2.length > 0) arg2 = arg2.toLowerCase();
 		if(arg3 && arg3.length > 0) arg3 = arg3.toLowerCase();
-		let info = config.list[data.job];
 		switch (arg1) {
 			case 'on':
-				if (info) {
+				arg3 = config.list[data.job];
+				if (arg3) {
 					config.enabled = true;
 					msg(`Auto-Shit: enabled.`);
 				} else {
@@ -21,7 +21,8 @@ module.exports = function autoShit(mod) {
 				}
 				break;
 			case 'off':
-				if (info) {
+				arg3 = config.list[data.job];
+				if (arg3) {
 					config.enabled = false;
 					msg(`Auto-Shit: disabled.`);
 				} else {
@@ -29,9 +30,9 @@ module.exports = function autoShit(mod) {
 				}
 				break;
 			case 'set':
-				arg3 = arg3.match(/#(\d*)@/) || 0;
-				arg3 = Number(arg3[1]) || 0;
-				if (arg3 <= 0) {
+				arg3 = arg3.match(/#(\d*)@/) || -1;
+				arg3 = Number(arg3[1]) || -1;
+				if (arg3 < 0) {
 					msg(`Auto-Shit: please put item link.`);
 					break;
 				}
@@ -40,7 +41,7 @@ module.exports = function autoShit(mod) {
 					case 'brooch':
 						msg(`Auto-Shit: brooch id set to ${arg3}.`);
 						config.list[data.job].broochinfo = arg3;
-						data.broochinfo = {id: arg3}
+						data.broochinfo = {id: arg3, amount: 1}
 						jsonSave('config.json', config);
 						break;
 					case 'r':
@@ -58,12 +59,23 @@ module.exports = function autoShit(mod) {
 			case 'load':
 				config = getConfig();
 				msg(`Auto-Shit: config has been reloaded.`);
+				arg3 = config.list[data.job];
+				if (arg3 && arg3.broochinfo > 0)
+					data.broochinfo = {id: arg3.broochinfo}
+				else
+					data.broochinfo = null;
+				if (arg3 && arg3.rootbeerinfo > 0)
+					data.rootbeerinfo = {id: arg3.rootbeerinfo}
+				else
+					data.rootbeerinfo = null;
 				break;
 			case 'info':
 			case 'check':
 			case 'debug':
-				msg(`Brooch Id: ${data.brooch.id || data.broochinfo.id}`);
-				msg(`Rootbeer Id: ${data.rootbeer || data.rootbeerinfo}`);
+				arg3 = data.brooch ? data.brooch.id : -1;
+				if (arg3 < 0) arg3 = data.broochinfo ? data.broochinfo.id : -1;
+				msg(`Brooch Id: ${arg3}`);
+				msg(`Rootbeer Id: ${data.rootbeer ? data.rootbeer.id : -1}`);
 				break;
 			default:
 				msg(`Auto-Shit: wrong commands :v`);
@@ -74,7 +86,15 @@ module.exports = function autoShit(mod) {
 	mod.hook('S_LOGIN', 12, e => {
 		data.gameId = e.gameId;
 		data.job = (e.templateId - 10101) % 100;
-		resetShit();
+		let info = config.list[data.job];
+		if (info && info.broochinfo > 0)
+			data.broochinfo = {id: info.broochinfo, amount: 1}
+		else
+			data.broochinfo = null;
+		if (info && info.rootbeerinfo > 0)
+			data.rootbeerinfo = {id: info.rootbeerinfo}
+		else
+			data.rootbeerinfo = null;
 	});
 	
 	mod.hook('C_PLAYER_LOCATION', 5, e => {
@@ -88,7 +108,14 @@ module.exports = function autoShit(mod) {
 	});
 	
 	mod.hook('S_RETURN_TO_LOBBY', 'raw', () => {
-		resetShit();
+		itemCd = {brooch: 0, rootbeer: 0};
+		data.usedRootbeer = false;
+		data.usedBrooch = false;
+		data.inbuff = false;
+		data.rootbeer = null;
+		data.brooch = null;
+		data.broochinfo = null;
+		data.rootbeerinfo = null;
 	});
 	
 	mod.hook('S_ABNORMALITY_BEGIN', 3, {order: Number.NEGATIVE_INFINITY}, e => {
@@ -117,12 +144,18 @@ module.exports = function autoShit(mod) {
 	mod.hook('S_START_COOLTIME_ITEM', 1, {order: Number.NEGATIVE_INFINITY}, e => {
  		if(!config.enabled) return;
  		if((data.brooch && e.item === data.brooch.id) || (data.broochinfo && e.item === data.broochinfo.id)) itemCd.brooch = Date.now() + e.cooldown * 1000;
- 		else if((data.rootbeer && e.item === data.rootbeer.id) || (data.rootbeerinfo && e.item === data.rootbeerinfo.id)) itemCd.rootbeer = Date.now() + e.cooldown * 1000;
+ 		else if(data.rootbeer && e.item === data.rootbeer.id) itemCd.rootbeer = Date.now() + e.cooldown * 1000;
  	});
 	
 	mod.hook('S_INVEN', mod.majorPatchVersion > 79 ? 18 : 17, e => {
-		data.brooch = e.items.find(item => item.slot === 20);
-		data.rootbeer = e.items.find(item => config.rootbeer.includes(item.id));
+		if (data.broochinfo)
+			data.brooch = e.items.find(item => item.id === data.broochinfo.id);
+		else
+			data.brooch = e.items.find(item => item.slot === 20);
+		if (data.rootbeerinfo)
+			data.rootbeer = e.items.find(item => item.id === data.rootbeerinfo.id);
+		else
+			data.rootbeer = e.items.find(item => config.rootbeer.includes(item.id));
 	});
 	
 	mod.hook('C_START_SKILL', 'raw', () => {checkShit();});
@@ -144,24 +177,6 @@ module.exports = function autoShit(mod) {
 		}
 	}
 	
-	function resetShit() {
-		let info = config.list[data.job];
-		if (info && info.broochinfo > 0)
-			data.broochinfo = {id: info.broochinfo}
-		else
-			data.broochinfo = null;
-		if (info && info.rootbeerinfo > 0)
-			data.rootbeerinfo = {id: info.rootbeerinfo}
-		else
-			data.rootbeerinfo = null;
-		itemCd = {brooch: 0, rootbeer: 0};
-		data.usedRootbeer = false;
-		data.usedBrooch = false;
-		data.inbuff = false;
-		data.rootbeer = null;
-		data.brooch = null;
-	}
-	
 	function startShit() {
 		if (!config.enabled || !data.inbuff || mod.game.me.inBattleground) return;
 		let info = config.list[data.job];
@@ -177,35 +192,35 @@ module.exports = function autoShit(mod) {
 						useItem(data.brooch || data.broochinfo);
 						break;
 				}
-			if (data.usedRootbeer && (data.rootbeer || data.rootbeerinfo) && now > itemCd.rootbeer)
+			if (data.usedRootbeer && data.rootbeer && now > itemCd.rootbeer)
 				switch (info.rootbeer.toLowerCase()) {
 					case 'once':
-						useItem(data.rootbeer || data.rootbeerinfo);
+						useItem(data.rootbeer);
 						data.usedRootbeer = true;
 						break;
 					case 'inbuff':
-						useItem(data.rootbeer || data.rootbeerinfo);
+						useItem(data.rootbeer);
 						break;
 				}
 		}
 	}
 	
 	function useItem(d) {
-		if (!d) return;
-		mod.send('C_USE_ITEM', 3, {
-			gameId: data.gameId,
-			id: d.id,
-			dbid: 0,
-			target: 0,
-			amount: 1,
-			dest: {x: 0, y: 0, z: 0},
-			loc: data.loc,
-			w: data.w,
-			unk1: 0,
-			unk2: 0,
-			unk3: 0,
-			unk4: true
-		});
+		if (d && d.amount > 0)
+			mod.send('C_USE_ITEM', 3, {
+				gameId: data.gameId,
+				id: d.id,
+				dbid: 0,
+				target: 0,
+				amount: 1,
+				dest: {x: 0, y: 0, z: 0},
+				loc: data.loc,
+				w: data.w,
+				unk1: 0,
+				unk2: 0,
+				unk3: 0,
+				unk4: true
+			});
 	}
 
 	function jsonRequire(d) {
